5c499ae1d1caf7fa62b579f1d69692be43d7a783
Atul Bharti <146771106+atulbhartiproff@users.noreply.github.com>
2024-09-02 03:40:15 +0530

    "tweeter start"

---
+++ tweeter.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.26;
+
+contract twwt{
+
+    mapping(address => string) public tweets;
+    
+    function createTweet(string memory _tweet) public{
+        tweets[msg.sender]= _tweet;
+    }
+
+    function getTweet(address _owner) public view returns (string memory) {
+        return tweets[_owner];
+    }
+}
