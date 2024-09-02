da131692aa7549a9f99c7ac344f8d08a9ee47eed
Atul Bharti <146771106+atulbhartiproff@users.noreply.github.com>
2024-09-03 02:23:35 +0530

    "Added likes/dislikes"

--- tweeter.sol
+++ tweeter.sol
@@ -4,21 +4,37 @@ pragma solidity ^0.8.26;
 
 contract twwt{
 
-    uint16 constant Max_tweet_length=250;
+    uint16 Max_tweet_length=250;
 
     struct tweet{
+        uint256 id;
         address author;
         string content;
         uint256 timestamp;
         uint128 likes;
     }
     mapping(address => tweet[]) public tweets;
-    
+    address public owner;
+
+    constructor(){
+        owner=msg.sender;
+    }
+
+    modifier  onlyOwner()  {
+        require(msg.sender== owner, "You are not the owner");
+        _;
+    }
+
+    function changeTweetLength(uint16 newTweetLength) public onlyOwner(){
+        Max_tweet_length=newTweetLength;
+    }
+
     function createTweet(string memory _tweet) public{
         
         require(bytes(_tweet).length<=Max_tweet_length,"Tweet is too long broski");
 
         tweet memory newtweet= tweet({
+            id:tweets[msg.sender].length,
             author:msg.sender,
             content:_tweet,
             timestamp: block.timestamp,
@@ -26,6 +42,17 @@ contract twwt{
         });
         
         tweets[msg.sender].push(newtweet);
+    }
+
+    function Like(address author, uint256 id) external {
+        require(tweets[author][id].id==id, "Tweet NOT IN EXISTENCE");
+        tweets[author][id].likes++;
+    }
+
+    function unLike(address author, uint256 id) external {
+        require(tweets[author][id].id==id, "Tweet NOT IN EXISTENCE");
+        require(tweets[author][id].likes>0, "Tweet does not have enough likes to dislike");
+        tweets[author][id].likes--;
     }
 
     function getTweet(uint _i) public view returns (tweet memory) {
